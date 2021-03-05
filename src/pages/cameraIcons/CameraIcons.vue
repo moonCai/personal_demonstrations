b7a06fb4bdd796d8acfd9bce2b64a2af6ae7cd8d
moonCai <moonchina_cai@126.com>
2021-03-05 13:46:59 +0800

    实现摄像头标识动画

--- src/pages/cameraIcons/CameraIcons.vue
+++ src/pages/cameraIcons/CameraIcons.vue
@@ -1,16 +1,58 @@
 <template>
   <div id="camera-icons">
+      <icon x="200" y="200" iconId="1" />
+      <!-- <icon x="130" y="120" iconId="2" /> 
+      <icon x="280" y="230" iconId="3" />
+      <icon x="330" y="204" iconId="4" />
+      <icon x="430" y="600" iconId="5" />
+      <icon x="940" y="120" iconId="6" />
+      <icon x="740" y="260" iconId="7" />
+      <icon x="640" y="660" iconId="8" />
+      <icon x="530" y="460" iconId="9" />
+      <icon x="840" y="600" iconId="10" />
+      <icon x="550" y="680" iconId="11"/>  -->
   </div>
 </template>
 
 <script>
+  import { defaultInitCesium } from "assets/js/cesium/mapInit";
   import { locationMixin } from "assets/js/mixin/mixin";
+
+  import Icon from "./Icon"
 
   export default {
     data() {
       return {};
     },
     mixins: [locationMixin],
+    components: {
+        Icon
+    },
+    mounted() {
+      this.mapViewer = defaultInitCesium("camera-icons", "tiandiTu", true);
+
+      this.mapViewer.camera.setView({
+        destination: Cesium.Rectangle.fromDegrees(...this.hubeiGeoRect),
+      });
+
+      this.leftClickOnMapEvent();
+    },
+    methods: {
+      leftClickOnMapEvent() {
+        let targetViewer = this.mapViewer;
+
+        let handler = new Cesium.ScreenSpaceEventHandler(
+          targetViewer.scene.canvas
+        );
+
+        handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+        handler.setInputAction((movement) => {
+            console.log("左键")
+        //   PopUp.popOver(targetViewer, movement);
+        }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+      },
+    },
   };
 </script>
 
---
+++ src/pages/cameraIcons/Icon.vue
@@ -0,0 +1,134 @@
+<template>
+  <div class="icon" :id="iconId">
+    <img class="camera" src="./images/video.png" />
+    <div class="line"></div>
+    <div class="item circle1"></div>
+    <div class="item circle2"></div>
+    <div class="item circle3"></div>
+    <div class="item circle4"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {};
+    },
+    props: ["y", "x", "iconId"],
+    mounted() {
+      $(`#${this.iconId}`).css({
+        top: `${this.y}px`,
+        left: `${this.x}px`,
+      });
+    },
+  };
+</script>
+
+<style>
+  .icon {
+    width: 60px;
+    height: 150px;
+    /* border: 1px solid coral; */
+    position: absolute;
+    z-index: 100;
+  }
+
+  .camera {
+    width: 45px;
+    position: absolute;
+    top: 10px;
+    left: 50%;
+    margin-left: -23px;
+    animation: jump 1.3s linear infinite forwards;
+  }
+
+  .line {
+    width: 4px;
+    height: 71px;
+    /* background-size: 8px 70px; */
+    border-radius: 2px;
+    background: url(./images/line.png) no-repeat center / cover;
+    /* background-image: url(./images/gradient.png); */
+    position: absolute;
+    top: 55px;
+    left: 50%;
+    margin-left: -2px;
+    margin-top: 0;
+    animation: jump 1.3s linear infinite forwards;
+  }
+
+  .item {
+    width: 10px;
+    height: 5px;
+    margin: auto;
+    border-radius: 50%;
+    background: rgba(228, 57, 96, 0.6);
+    position: absolute;
+    left: 50%;
+    top: 130px;
+    margin-left: -5px;
+  }
+
+  .circle2 {
+    opacity: 0;
+    animation: spread1 1.3s linear infinite;
+  }
+
+  .circle3 {
+    opacity: 0;
+    animation: spread1 1.3s linear 0.2s infinite;
+  }
+
+  .circle4 {
+    opacity: 0;
+    animation: spread1 1.3s linear 0.4s infinite;
+  }
+
+  @keyframes spread1 {
+    0% {
+      transform: scale(1);
+      opacity: 0;
+    }
+
+    50% {
+      transform: scale(3);
+      opacity: 0.6;
+    }
+
+    100% {
+      transform: scale(6);
+      opacity: 0.1;
+    }
+  }
+
+  @keyframes jump {
+    0% {
+      transform: translateY(0);
+    }
+
+    50% {
+      transform: translateY(-10px);
+    }
+
+    100% {
+      transform: translateY(0);
+    }
+  }
+
+  @keyframes jump1 {
+    0% {
+      margin-top: 0;
+      background-position-x: 0px;
+    }
+
+    50% {
+      margin-top: -10px;
+      background-position-x: 4px;
+    }
+
+    100% {
+      margin-top: 0;
+      background-position-x: 8px;
+    }
+  }
+</style>
